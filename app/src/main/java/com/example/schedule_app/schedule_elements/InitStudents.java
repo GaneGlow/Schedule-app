4ffb30ad6a9682cf80551862b0d1b5da278d8f99
Gane Glow <xxxsofochka@mail.com>
2025-11-06 14:24:30 +0300

    Add class for student initialization

---
+++ app/src/main/java/com/example/schedule_app/schedule_elements/InitStudents.java
@@ -0,0 +1,20 @@
+package com.example.schedule_app.schedule_elements;
+
+public class InitStudents {
+
+    public int group;
+    public int course;
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public InitStudents(int group, int course) {
+        this.group = group;
+        this.course = course;
+    }
+}
